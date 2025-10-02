8a4093e8b6dc0a7492356d0ec9ac5351ddc770e4
aminnaghavi <60716356+aminnaghavi@users.noreply.github.com>
2025-10-02 11:15:03 +0200

    Update card_data.php

--- htdocs/card_data.php
+++ htdocs/card_data.php
@@ -3,7 +3,7 @@
 	if(strcmp($id,"EA04A75910001")==0)
 		echo "GRANTED"."\n"."AMIN\n";
 	else if(strcmp($id,"4E5E7985EC0001")==0)
-		echo "GRANTED"."\n"."9227403\n";
+		echo "GRANTED"."\n"."9999999\n";
 	else
 		echo "DENAID";
 ?>
