e31a6f7e3172e0b08d7f49256b4e59f66d8920e0
aminnaghavi <60716356+aminnaghavi@users.noreply.github.com>
2025-10-02 11:14:26 +0200

    Update db_fns.php

--- htdocs/db_fns.php
+++ htdocs/db_fns.php
@@ -1,7 +1,7 @@
 <?php
 function db_conn()
 {
-	@ $db = new mysqli('localhost','root','113733','users');
+	@ $db = new mysqli('localhost','root','123456','users');
 	if(mysqli_connect_errno())
 		throw new Exception('Error! Cannot connect to database!',100);
 	return $db;
